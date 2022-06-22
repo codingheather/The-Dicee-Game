59a0e3684b94284d991f7e8c20fe282472b12e34
Heather Hao <89426050+codingheather@users.noreply.github.com>
2022-06-22 15:18:02 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+
+var randomImageSource1 = "images/" + randomDiceImage1;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🍓Player 1 Wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🍓";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
